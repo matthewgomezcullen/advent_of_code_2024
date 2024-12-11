9f1697f7541448a31c9ec0b2e3a4fbf5e96737bc
Matthew Gomez Cullen <mattgc1282@hotmail.co.uk>
2024-12-11 15:15:15 +0000

    Solve day 4 part 1

--- day_4/part_1.py
+++ day_4/part_1.py
@@ -1,44 +1,68 @@
 import re
 
 
-def read_input(filename: str) -> str:
+def read_input(filename: str) -> list[str]:
     with open(filename, "r") as f:
-        return f.read()
+        return f.readlines()
 
 
-def solution(s: str) -> int:
-    line_length = s.index("\n")
-    s = s.replace("\n", "")
-    print(line_length, s)
-    xmas_regex = (
-        r"XMAS|SAMX|"
-        r"X" + "." * (line_length) + "M" + "." * (line_length) + "A" + "." * (line_length) + "S|"
-        r"S" + "." * (line_length) + "A" + "." * (line_length) + "M" + "." * (line_length) + "X|"
-        r"X" + "." * (line_length + 1) + "M" + "." * (line_length + 1) + "A" + "." * (line_length + 1) + "S|"
-        r"S" + "." * (line_length + 1) + "A" + "." * (line_length + 1) + "M" + "." * (line_length + 1) + "X|"
-        r"X" + "." * (line_length - 1) + "M" + "." * (line_length - 1) + "A" + "." * (line_length - 1) + "S|"
-        r"S" + "." * (line_length - 1) + "A" + "." * (line_length - 1) + "M" + "." * (line_length - 1) + "X"
-    )
-    print(xmas_regex)
-    matches = re.findall(xmas_regex, s)
-    print(matches)
-    return len(matches)
-    
+def solution(lines: list[str]) -> int:
+    line_length = len(lines[0])
+
+    def matches(row: int, col) -> int:
+        count = 0
+        if col + 3 < line_length and lines[row][col : col + 4] in ["XMAS", "SAMX"]:
+            count += 1
+        if row + 3 < len(lines) and "".join(
+            [lines[row + i][col] for i in range(4)]
+        ) in ["XMAS", "SAMX"]:
+            count += 1
+        if (
+            row + 3 < len(lines)
+            and col + 3 < line_length
+            and "".join([lines[row + i][col + i] for i in range(4)]) in ["XMAS", "SAMX"]
+        ):
+            count += 1
+        if (
+            row + 3 < len(lines)
+            and col - 3 >= 0
+            and "".join([lines[row + i][col - i] for i in range(4)]) in ["XMAS", "SAMX"]
+        ):
+            count += 1
+        return count
+
+    # xmas_regex = (
+    #     r"XMAS|SAMX|"
+    #     r"X" + "." * (line_length) + "M" + "." * (line_length) + "A" + "." * (line_length) + "S|"
+    #     r"S" + "." * (line_length) + "A" + "." * (line_length) + "M" + "." * (line_length) + "X|"
+    #     r"X" + "." * (line_length + 1) + "M" + "." * (line_length + 1) + "A" + "." * (line_length + 1) + "S|"
+    #     r"S" + "." * (line_length + 1) + "A" + "." * (line_length + 1) + "M" + "." * (line_length + 1) + "X|"
+    #     r"X" + "." * (line_length - 1) + "M" + "." * (line_length - 1) + "A" + "." * (line_length - 1) + "S|"
+    #     r"S" + "." * (line_length - 1) + "A" + "." * (line_length - 1) + "M" + "." * (line_length - 1) + "X"
+    # )
+    count = 0
+    for row in range(len(lines)):
+        for col in range(line_length):
+            count += matches(row, col)
+    return count
 
 
 if __name__ == "__main__":
-    # print(solution(read_input("inp/day_4.txt")))
-    print(
-        solution(
-"""MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX"""
-        )
-    )
+    print(solution(read_input("inp/day_4.txt")))
+    # print(
+    #     solution(
+    #         [
+    #             "MMMSXXMASM",
+    #             "MSAMXMSMSA",
+    #             "AMXSXMAAMM",
+    #             "MSAMASMSMX",
+    #             "XMASAMXAMM",
+    #             "XXAMMXXAMA",
+    #             "SMSMSASXSS",
+    #             "SAXAMASAAA",
+    #             "MAMMMXMMMM",
+    #             "MXMXAXMASX",
+    #         ]
+    #     )
+    # )
+    # answer: 18
